e2a00581cd89a931d412cb15690875311338e1c8
Milf4ch <bioqub@yandex.ru>
2024-01-17 15:19:07 +0300

    Add user tag at base.html

--- app/goods/urls.py
+++ app/goods/urls.py
@@ -5,6 +5,6 @@ app_name = 'goods'
 
 urlpatterns = [
 
-    path('', catalog, name='index'),
-    path('product/<int:product_id>', product, name='product'),
+    path('<slug:cat_slug>', catalog, name='index'),
+    path('product/<slug:product_slug>', product, name='product'),
 ]
--- app/goods/views.py
+++ app/goods/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 from goods.models import Category, Product
 
 
-def catalog(request):
+def catalog(request, cat_slug):
 
-    goods = Product.objects.all()
+    if cat_slug=='all':
+        goods = Product.objects.all()
+    else:
+        goods = Product.objects.filter(category__slug=cat_slug)
 
     context = {
         'title' : 'Каталог товаров',
@@ -13,9 +16,9 @@ def catalog(request):
         }
     return render(request, 'goods/catalog.html', context)
 
-def product(request, product_id):
+def product(request, product_slug):
 
-    product = Product.objects.get(id=product_id)
+    product = Product.objects.get(slug=product_slug)
 
     context = {
         'product' : product,
